7b415e8de6dba01c399733da0eb4042cea9238df
Allan <allandouglas@live.com>
2020-12-27 02:22:23 -0300

    fix OpCode4XNN executer

--- Chip8Console/Src/CPU/Executers/OpCode4XNN.cs
+++ Chip8Console/Src/CPU/Executers/OpCode4XNN.cs
@@ -13,14 +13,13 @@ namespace Chip8Console.CPU
         {
             var x = (ushort)((opcode.value & 0x0F00) >> 8);
             var Vx = cpu.GetFromRegister(x);
-            var constant = 0x0FF;
+            var constant = opcode.value & 0x0FF;
 
             if (Vx != constant)
             {
                 cpu.ProgramCounter += 2;
             }
 
-            cpu.ProgramCounter += 2;
         }
     }
 }
