6450b60208ce58f567d252928174a3d7184e7b2a
Allan <allandouglas@live.com>
2020-12-27 02:24:27 -0300

    add OpCodeExecuter FX65

---
+++ Chip8Console/Src/CPU/Executers/OpCodeFX65.cs
@@ -0,0 +1,22 @@
+namespace Chip8Console.CPU
+{
+    public class OpCodeFX65 : AOpCodeExecuter
+    {
+        public OpCodeFX65(ICPU cpu) : base(cpu)
+        {
+        }
+
+        public override OpCode Filter => new(0xF0FF);
+        public override OpCode OpCode => new(0xF065);
+        public override void Execute(OpCode opcode)
+        {
+
+            var x = (opcode.value & 0x0f00) >> 8;
+
+            for (ushort i = 0; i <= x; i++)
+            {
+                cpu.StoreIntoRegister(i, cpu.Memory.Read((ushort)(cpu.RegisterI + i)));
+            }
+        }
+    }
+}
