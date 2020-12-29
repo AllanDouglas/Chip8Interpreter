da38fd68dec2711636cd94c4e038efcd1e705348
Allan <allandouglas@live.com>
2020-12-29 17:24:30 -0300

    fix ExA1 OpCode

--- Chip8Console/Src/CPU/Executers/OpCodeEXA1.cs
+++ Chip8Console/Src/CPU/Executers/OpCodeEXA1.cs
@@ -12,7 +12,7 @@ namespace Chip8Console.CPU
 
         public override void Execute(OpCode opcode)
         {
-            var x = opcode.value & 0x0F00;
+            var x = (opcode.value & 0x0F00) >> 8;
             var vx = cpu.Registers[x];
 
             if (vx < cpu.Keyboard.Keys.Length && cpu.Keyboard.Keys[vx] == 0)
